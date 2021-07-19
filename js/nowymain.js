991b8a3ab25dbbc9c2084eb44475365e2badbddd
oc <oskar.cejman@nxo.pl>
2021-07-19 12:49:28 +0200

    Próba gnerowania wiecej niz jednej osoby

--- js/nowymain.js
+++ js/nowymain.js
@@ -11,10 +11,10 @@ function myFunction(){
     .then(function(res){
     console.log(res)
 
-    let fin = res.find( record => record.name === data || record.miasto === data)
+    let fin = res.filter( record => record.name === data || record.miasto === data)
     let visible = document.querySelector('.lowerWrapper');
     visible.style.display = "block";
-    if(fin){
+    if(fin.length === 1){
     console.log(fin)
     document.querySelector('.lowerWrapper').innerHTML = ' ';
     let createDiv = document.createElement('div');
@@ -23,6 +23,58 @@ function myFunction(){
     let imagine = document.querySelector('.divv');
     imagine.style.float = "right";
     imagine.innerHTML = fin.zdjecie;
+    
+
+        let createFirstDiv = document.createElement('div');
+        createFirstDiv.classList = 'tekst0';
+        document.querySelector('.lowerWrapper').appendChild(createFirstDiv);
+
+        let createSecondDiv = document.createElement('div');
+        createSecondDiv.classList = 'tekst';
+        document.querySelector('.lowerWrapper').appendChild(createSecondDiv); 
+
+        let createFourthDiv = document.createElement('div');
+        createFourthDiv.classList = 'tekst4';
+        document.querySelector('.lowerWrapper').appendChild(createFourthDiv);
+
+        let createFivethDiv = document.createElement('div');
+        createFivethDiv.classList = 'tekst5';
+        document.querySelector('.lowerWrapper').appendChild(createFivethDiv);
+
+        let createSixthDiv = document.createElement('div');
+        createSixthDiv.classList = 'tekst6';
+        document.querySelector('.lowerWrapper').appendChild(createSixthDiv);
+
+        let createThirdDiv = document.createElement('iframe');
+        createThirdDiv.classList = 'tekst3';
+        createThirdDiv.src = fin.link;
+        createThirdDiv.width = '620px';
+        createThirdDiv.height = '360px';
+        createThirdDiv.frameBorder = 0;
+        createThirdDiv.gesture = 'media';
+        createThirdDiv.allow = 'encrypted-media';
+        createThirdDiv.allowFullscreen;
+        createThirdDiv.style.marginTop = '30px'
+        document.querySelector('.lowerWrapper').appendChild(createThirdDiv);
+
+        
+        document.querySelector('.tekst').textContent = 'Pseudonim: ' + fin.name;    
+        document.querySelector('.tekst4').textContent = 'Skąd pochodzi: ' + fin.miasto;
+        document.querySelector('.tekst5').textContent = 'Data urodzenia: ' + fin.data_urodzenia;
+        document.querySelector('.tekst6').textContent = 'Fakty: ' + fin.historia;
+        // document.querySelector('.tekst3').innerHTML = ' <iframe src="' + fin.link + '"width="420" height="315"></iframe>'
+        
+        
+    }
+    else if(fin.length > 1){
+        fin.forEach(function(item, index){
+        console.log('Wypisuje '+ item.name)
+        let createDiv = document.createElement('div');
+        createDiv.classList = 'divv';
+        document.querySelector('.lowerWrapper').appendChild(createDiv);
+        let imagine = document.querySelector('.divv');
+        imagine.style.float = "right";
+        imagine.innerHTML = item.zdjecie;
     
 
         let createFirstDiv = document.createElement('div');
@@ -49,7 +101,7 @@ function myFunction(){
 
         let createThirdDiv = document.createElement('iframe');
         createThirdDiv.classList = 'tekst3';
-        createThirdDiv.src = fin.link;
+        createThirdDiv.src = item.link;
         createThirdDiv.width = '620px';
         createThirdDiv.height = '360px';
         createThirdDiv.frameBorder = 0;
@@ -60,14 +112,12 @@ function myFunction(){
     
 
         document.querySelector('.lowerWrapper').appendChild(createThirdDiv);
-        document.querySelector('.tekst0').textContent = 'Imię i Nazwisko: ' + fin.realname;
-        document.querySelector('.tekst').textContent = 'Pseudonim: ' + fin.name;    
-        document.querySelector('.tekst4').textContent = 'Skąd pochodzi: ' + fin.miasto;
-        document.querySelector('.tekst5').textContent = 'Data urodzenia: ' + fin.data_urodzenia;
-        document.querySelector('.tekst6').textContent = 'Fakty: ' + fin.historia;
-        // document.querySelector('.tekst3').innerHTML = ' <iframe src="' + fin.link + '"width="420" height="315"></iframe>'
-        
-        
+        document.querySelector('.tekst0').textContent = 'Imię i Nazwisko: ' + item.realname;
+        document.querySelector('.tekst').textContent = 'Pseudonim: ' + item.name;    
+        document.querySelector('.tekst4').textContent = 'Skąd pochodzi: ' + item.miasto;
+        document.querySelector('.tekst5').textContent = 'Data urodzenia: ' + item.data_urodzenia;
+        document.querySelector('.tekst6').textContent = 'Fakty: ' + item.historia;
+        })
     }
     else{
         
@@ -93,4 +143,5 @@ function myFunction(){
          myFunction()
         }
         else{}
-})   
+})
+   
